70d7aacffa543089e01b095a56c971eb9cab56d2
Vatan Aksoy Tezer <vatanaksoytezer@gmail.com>
2020-05-24 21:03:34 +0300

    Change plot axis bounds

--- src/main/kotlin/org/ghrobotics/falcondashboard/logger/charts/RPMChart.kt
+++ src/main/kotlin/org/ghrobotics/falcondashboard/logger/charts/RPMChart.kt
@@ -5,7 +5,6 @@ import javafx.scene.chart.NumberAxis
 import javafx.scene.chart.XYChart
 import org.ghrobotics.falcondashboard.FalconDs.RPM
 import org.ghrobotics.falcondashboard.FalconDs.matchTime
-import javafx.application.Platform;
 import org.ghrobotics.falcondashboard.ui
 
 
@@ -14,7 +13,7 @@ object RPMChart : LineChart <Number,Number>(
         NumberAxis()
 ) {
     private val dataList = XYChart.Series<Number,Number>()
-    private val WINDOW_SIZE = 100
+    private val WINDOW_SIZE = 10
 
     init{
         this.animated = false
@@ -30,7 +29,22 @@ object RPMChart : LineChart <Number,Number>(
             if (dataList.data.size > WINDOW_SIZE) {
                 dataList.data.removeAt(0);
             }
+            setAxisBounds(this, dataList.data[0].xValue.toDouble(),
+                dataList.data[dataList.data.lastIndex].xValue.toDouble()*1.0, true)
         }
 
+    }
+
+    fun setAxisBounds(
+        myChart: RPMChart,
+        min: Double,
+        max: Double,
+        isXAxis: Boolean
+    ) {
+        val axis: NumberAxis
+        axis = if (isXAxis) myChart.xAxis as NumberAxis else myChart.yAxis as NumberAxis
+        axis.isAutoRanging = false
+        axis.lowerBound = min
+        axis.upperBound = max
     }
 }
